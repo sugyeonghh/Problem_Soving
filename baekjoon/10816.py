507a9480833e6ea405c4b8f923efb4e187ec840a
sugyeonghh <sugyeong.py@gmail.com>
2022-01-05 05:44:23 +0900

    upload

---
+++ baekjoon/10816.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+n = int(input())
+card_1 = list(map(int, stdin.readline().split()))
+m = int(input())
+card_2 = list(map(int, stdin.readline().split()))
+res = dict()
+
+for i in card_1:
+	if i in res:
+		res[i] += 1
+	else:
+		res[i] = 1
+
+for i in card_2:
+	if i in res:
+		print(res[i], end = ' ')
+	else:
+		print(0, end = ' ')
---
+++ baekjoon/14501.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(input())
+t = [0]
+p = [0]
+dp = [0] * (n + 2)
+
+for i in range(n):
+	a, b = map(int, sys.stdin.readline().split())
+	t.append(a)
+	p.append(b)
+
+for i in range(1, n + 2):
+	idx = i + t[i]
+	if (idx <= n):
+		dp[idx] = max(dp[idx], dp[i] + p[i])
+	print(dp)
+print(max(dp))
