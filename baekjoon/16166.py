d3ef7e88b930b5dea2799d06fd0d4ebd65002879
sugyeonghh <sugyeong.py@gmail.com>
2022-10-12 01:40:54 +0900

    FINALLY finished 16166 BOJ

--- baekjoon/16166.py
+++ baekjoon/16166.py
@@ -1,50 +1,39 @@
 # 서울의 지하철
-# 그래프 탐색 / bfs / dijkstra
+# 그래프 탐색 / bfs
 
 from sys import stdin
-from collections import deque
+
+def bfs(start, cnt):
+	togo = []
+	for l in station[start]:
+		for y in line[l]:
+			if y == dest:
+				return cnt
+			if y not in visited:
+				if len(station[y]) > 1:
+					togo.append(y)
+				visited.append(y)
+	for x in togo:
+		return bfs(x, cnt + 1)
+
 
 n = int(stdin.readline().strip())
-
-# make graph
-graph1 = []
-graph2 = []
-line_num = []
-visited = {}
-for l in range(n):
+visited = []
+line = {}
+station = {}
+for line_num in range(1, n + 1):
 	tmp = list(map(int, stdin.readline().split()))
-	num = tmp[0]
-	line = tmp[1:]
-	for i in range(1, num):
-		a1 = line[i - 1]
-		a2 = line[i]
-		if a1 in graph1:
-			idx = graph1.index(a1)
-			graph2[idx].append(a2)
-			visited[a2] = 0
-			line_num[idx].append(l + 1)
+	line[line_num] = tmp[1:]
+	for a in tmp[1:]:
+		if a in station:
+			station[a].append(line_num)
 		else:
-			graph1.append(a1)
-			graph2.append([a2])
-			visited[a1] = 0
-			visited[a2] = 0
-			line_num.append([l + 1])
+			station[a] = [line_num]
 
-arrival = int(stdin.readline().strip())
-start = 0
-result = 0
-
-queue = deque()
-visited[start] = 1
-queue.append(start)
-while queue:
-	v = queue.popleft()
-	if v in graph1:
-		idx = graph1.index(v)
-		for a in graph2[idx]:
-			if a == arrival:
-				break
-			if a and not visited[a]:
-				visited[a] = 1
-				queue.append(a)
-
+dest = int(stdin.readline().strip())
+visited.append(0)
+cnt = bfs(0, 0)
+if cnt == None:
+	print(-1)
+else:
+	print(cnt)
