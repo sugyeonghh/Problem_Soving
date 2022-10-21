a92e28e4fd4473e472524d5549415c78de0ab622
sugyeonghh <sugyeong.py@gmail.com>
2022-10-22 02:28:14 +0900

    upload 1107

--- baekjoon/1107.py
+++ baekjoon/1107.py
@@ -1,7 +1,22 @@
-N = int(input())
-M = int(input())
-if M:
-	buttons = int(input().split())
+from sys import stdin
 
-if N == 100 or M == 0:
-	print(1)
+MAX = 10 ** 6
+dest = int(input())
+num_of_broken = int(input())
+broken = []
+
+if num_of_broken:
+	broken = stdin.readline().split()
+
+if dest == 100:
+	print(0)
+else:
+	moves = abs(dest - 100)
+	for channel_int in range(MAX + 1):
+		channel = str(channel_int)
+		for idx, ch in enumerate(channel):
+			if ch in broken:
+				break
+			elif idx == len(channel)-1:
+				moves = min(moves, abs(dest-channel_int)+len(channel))
+	print(moves)
