ec66c32cd47bb6fd7e02ea9780e493d6a9b4ef08
sugyeonghh <sugyeong.py@gmail.com>
2023-05-11 04:50:12 +0900

    baekjoon-dfs/bfs 스타트링크

--- baekjoon/5014.py
+++ baekjoon/5014.py
@@ -3,18 +3,21 @@ from collections import deque
 
 def bfs():
 	queue = deque([s])
+	visited[s] = 1
 	while queue:
 		vertex = queue.popleft()
 		if vertex == g:
 			return graph[g]
 		for pos in [vertex + u, vertex - d]:
-			if 0 < pos <= f and not graph[pos]:
+			if 0 < pos <= f and not visited[pos]:
+				visited[pos] = 1
 				graph[pos] = graph[vertex] + 1
 				queue.append(pos)
 	return -1
 
 f, s, g, u, d = map(int, stdin.readline().split())
 graph = [0] * (f + 1)
+visited = [0] * (f + 1)
 
 answer = bfs()
 print(answer if answer != -1 else 'use the stairs')
