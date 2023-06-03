1cec9693a24396b9947be31e237c21712ef3e1b0
sugyeonghh <sugyeong.py@gmail.com>
2023-06-04 03:15:54 +0900

    programmers-L,R indexing

--- programmers/연속된 부분 수열의 합.py
+++ programmers/연속된 부분 수열의 합.py
@@ -1,14 +1,17 @@
 def solution(sequence, k):
     l = r = 0
     answer = [0, len(sequence)]
-    
-    while l >= 0 and r <= len(sequence)+1:
-        if sum(sequence[l:r]) < k:
+    sum = sequence[0]
+
+    while True:
+        if sum < k:
             r += 1
-        elif sum(sequence[l:r]) > k:
-            l += 1
+            if r == len(sequence): break
+            sum += sequence[r]
         else:
-            if r-l < answer[1]-answer[0]-1:
-                answer = [l, r-1]
+            if sum == k:
+                if r-l < answer[1]-answer[0]:
+                    answer = [l, r]
+            sum -= sequence[l]
             l += 1
     return answer
