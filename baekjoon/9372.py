594a312d8078d20e71af89adf9f784898a1d3881
sugyeonghh <sugyeong.py@gmail.com>
2023-06-08 19:27:13 +0900

    programmers-graph/MST

---
+++ baekjoon/9372.py
@@ -0,0 +1,7 @@
+from sys import stdin
+
+for i in range(int(input())):
+    n, m = map(int, stdin.readline().split())
+    for j in range(m):
+        a, b = map(int, stdin.readline().split())
+    print(n - 1)
