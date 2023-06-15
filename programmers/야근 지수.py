3e5431fda5415bdd645733a4e13bb6b03aa77be9
sugyeonghh <sugyeong.py@gmail.com>
2023-06-15 19:30:19 +0900

    programmers-priority queue/heap

--- programmers/야근 지수.py
+++ programmers/야근 지수.py
@@ -3,8 +3,6 @@ from heapq import heappush, heappop, heapify
 def solution(n, works):
     heapify(max_heap := [-i for i in works])
     
-    print(max_heap)
-    
     while n:
         if max_heap[0] == 0: return 0
         max = heappop(max_heap)
