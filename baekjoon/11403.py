72417006c44db7482fe0bd874172294866beffba
sugyeonghh <sugyeong.py@gmail.com>
2023-06-30 00:14:08 +0900

    baekjoon-floyd warshall

---
+++ baekjoon/11403.py
@@ -0,0 +1,22 @@
+from sys import stdin
+from sys import maxsize
+from collections import deque
+
+INF = maxsize
+N = int(stdin.readline())
+graph = []
+visited = [0] * N
+
+for i in range(N):
+    input = list(map(int, stdin.readline().replace('0', str(INF)).split()))
+    graph.append(input)
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in graph:
+    for j in i:
+        print(0, end=' ') if j == INF else print(1, end=' ')
+    print()
