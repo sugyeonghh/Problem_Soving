6f3adad7910258397a45554b908a5326e46d98ec
sugyeonghh <sugyeong.py@gmail.com>
2023-07-20 01:03:13 +0900

    baekjoon-MST

---
+++ baekjoon/1197.py
@@ -0,0 +1,29 @@
+from sys import stdin
+from heapq import heappop, heappush, heapify
+
+V, E = map(int, stdin.readline().split())
+
+visited = [0] * (V + 1)
+graph = [[] for i in range(V + 1)]
+
+for i in range(E):
+    a, b, v = map(int, stdin.readline().split())
+    graph[a].append((v, b))
+    graph[b].append((v, a))
+
+q = []
+heappush(q, (0, 1))
+cnt = 0
+answer = 0
+while q:
+    if cnt == V:
+        break
+    value, vertex = heappop(q)
+    if not visited[vertex]:
+        visited[vertex] = 1
+        cnt += 1
+        answer += value
+        for i in graph[vertex]:
+            heappush(q, i)
+
+print(answer)
